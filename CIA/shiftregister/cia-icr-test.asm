4041d7fb0ab35b904805a69f6df49ddae67a0e3f
groepaz <gpz@users.sourceforge.net>
2015-03-20 01:59:58 +0000

    updated
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/testprogs@29497 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- CIA/shiftregister/cia-icr-test.asm
+++ CIA/shiftregister/cia-icr-test.asm
@@ -9,6 +9,14 @@
           lda #$93
           jsr $ffd2
 
+          sei
+          ; make sure screen is disabled, so timing is not disturbed by badlines
+          lda #$0b
+          sta $d011
+          lda #$f0
+-         cmp $d012
+          bne -
+
           ldx #0
 tlp
           lda text,x
@@ -18,23 +26,22 @@ tlp
           bne tlp
 tsk
 
-          sei
           lda #0
           STA $DC0E           ; stop CIA Timer A
-          lda #$7f
+          lda #$7f            ; clear all interrupt flags
           sta $dc0d
           LDA $DC0D           ; read and clear the "interrupt control register"
 
-          lda #$81
+          lda #$81            ; enable timer A interrupts
           sta $dc0d
 
           LDA #0
           TAX                 ; Counter X = 0
           STA $DC05
-          
+
           LDA #1
           STA $DC04           ; Timer A value = 1
-          
+
           LDA #2
           STA $D020           ; border = red
 
@@ -46,9 +53,6 @@ tsk
           lda #%11010001
           sta $dc0e
           }
-                              
-;          LDA #$A8            ; write byte in "serial data register"
-;          STA $DC0C           ; and start transfer
           
 loop:
           !if (ONESHOT=1) {
@@ -64,31 +68,16 @@ loop:
 
           lda $dc0d           ; restore value from "interrupt control register"
           sta $0400+(10*40),x
-;          AND #8              ; Interrupt from SP?
-;          BEQ loop            ; no, then do it again
-                              ; --> endless loop in standalone/dockingstation mode
-;          LDA #7
-;          STA $D020           ; SP interrupt occured: border = yellow
 
-;          !if (ONESHOT=1) {
-;          CPX #16             ; 16 loops? (2 x 8-Bit)
-;          } else {
-;          cpx #2
-;          }
-;          BNE notok
           inx
           bne loop
-                           
+
 OK:       LDA #5
           STA $D020           ; test ok: border = green
-
-;notok:    LDA #0
-;          JSR $BDCD           ; print value X/A on screen
 
           lda #$7f
           sta $dc0d
           lda $dc0d
-          sei
 
           ldx #0
 lp1:
@@ -97,6 +86,7 @@ lp1:
           cmp reference,x
           beq sk1
           ldy #10
+          sty $d020
 sk1:
           tya
           sta $d800,x
@@ -110,6 +100,7 @@ lp2:
           cmp reference+$100,x
           beq sk2
           ldy #10
+          sty $d020
 sk2:
           tya
           sta $d800+$100,x
@@ -123,11 +114,15 @@ lp3:
           cmp reference+$200,x
           beq sk3
           ldy #10
+          sty $d020
 sk3:
           tya
           sta $d800+$200,x
           inx
           bne lp3
+
+          lda #$1b
+          sta $d011
 
           JMP *
 
