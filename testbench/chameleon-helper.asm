8e3456d25ad83b4d65931fd515605b7a6d3ea23c
groepaz <gpz@users.sourceforge.net>
2016-02-26 23:34:32 +0000

    updated testbench
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/testprogs@30640 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- testbench/chameleon-helper.asm
+++ testbench/chameleon-helper.asm
@@ -26,5 +26,34 @@ link:   !word 0
         dec $d020
         lda #0
         sta $d7ff
+        ; try to somewhat reset the TOD
+        ldx #0
+        ; cia 1 tod
+        stx $dc08 ; 0 starts TOD clock
+        stx $dc09 ; 0
+        stx $dc0a ; 0
+
+        ; cia 2 tod
+        stx $dd08 ; 0 starts TOD clock
+        stx $dd09 ; 0
+        stx $dd0a ; 0
+        inx
+        stx $dc0b ; 1 (hour) stops TOD clock
+        stx $dd0b ; 1 (hour) stops TOD clock
+
+        lda $dc08 ; un-latch
+        lda $dd08 ; un-latch
+
+        ; clear pending flags
+        lda $d01e ; Sprite to Sprite Collision Detect
+        lda $d01f ; Sprite to Background Collision Detect
+
+        ; clear pending irqs
+        lda $d019
+        sta $d019
+        lda $dc0d
+        lda $dd0d
+        lda $df00 ; REU
+
         cli
         rts
