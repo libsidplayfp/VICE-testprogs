a87c6621cc4a9cb76041c49b38db5814053a4044
groepaz <gpz@users.sourceforge.net>
2016-08-08 13:54:47 +0000

    updated test to check all registers
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/testprogs@31539 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- SID/busvalue/bus_value_test.asm
+++ SID/busvalue/bus_value_test.asm
@@ -7,28 +7,48 @@
 bend:       !word 0
 ;-------------------------------------------------------------------------------
 
+    ldy #$18
+loop1:
 ;write to reg
     lda #$a5
     sta $d410
 ;read from reg
     lda $d41B
     sta result
-;read from invalid reg
-    lda $d41F
+;read from write-only regs
+    lda $d400,y
     cmp result
-    beq ok
-nok:
-    lda #2
-    jmp prnt 
+    bne nok
+    dey
+    bne loop1
 
+    ldy #$2
+loop2:
+;write to reg
+    lda #$a5
+    sta $d410
+;read from reg
+    lda $d41B
+    sta result
+;read from non-existing regs
+    lda $d41D,y
+    cmp result
+    bne nok
+    dey
+    bne loop2
 ok:
     lda #5
+    jmp prnt
+
+nok:
+    lda #2
 
 prnt:
     sta $D020
 
     ldy #0      ; success
     lda $d020
+    and #$0f
     cmp #5
     beq +
     ldy #$ff    ; failure
