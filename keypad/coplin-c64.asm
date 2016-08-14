fe4445487ec159ea4302949a18760391c8d4d566
Marco van den Heuvel <blackystardust@users.sourceforge.net>
2016-08-14 23:11:08 +0000

    Rewrote the c64 coplin keypad test program in asm, makes it modular and easier to 'port' to the other emulators. Also added support for other ports (native 2 and userport adapters).
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/testprogs@31598 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ keypad/coplin-c64.asm
@@ -0,0 +1,818 @@
+*=$0801
+
+C64_CIA1_PRA = 0xDC00
+C64_CIA1_PRB = 0xDC01
+C64_CIA1_DDRA = 0xDC02
+C64_CIA1_TIMER_A_LOW = 0xDC04
+C64_CIA1_TIMER_A_HIGH = 0xDC05
+C64_CIA1_SR = 0xDC0C
+C64_CIA1_CRA = 0xDC0E
+C64_CIA2_PRA = 0xDD00
+C64_CIA2_DDRA = 0xDD02
+C64_CIA2_TIMER_A_LOW = 0xDD04
+C64_CIA2_TIMER_A_HIGH = 0xDD05
+C64_CIA2_SR = 0xDD0C
+C64_CIA2_CRA = 0xDD0E
+USERPORT_DDR = 0xDD03
+USERPORT_DATA = 0xDD01
+
+basic: !by $0b,$08,$01,$00,$9e,$32,$30,$36,$31,$00,$00,$00
+
+start:
+	ldx #$00
+	stx $D020	; border
+	stx $D021	; background
+	inx
+	stx $0286	; cursor
+
+mainloop:
+	jsr print_main_screen
+	jsr print_test_name_screen
+	jsr print_joy_device_screen
+	jsr print_press_f1_screen
+	lda #$00
+	sta $c6		; keys in buffer
+check_loop:
+	jsr check_f1
+	jsr check_port
+	jsr show_key
+	jmp check_loop
+
+show_key:
+	ldy #$00
+	ldx #$00
+	cmp #26
+	bne check_key_8
+	ldy #42
+	bne invert_key_4
+check_key_8:
+	cmp #30
+	bne check_key_9
+	ldy #46
+	bne invert_key_4
+check_key_9:
+	cmp #22
+	bne check_key_4
+	ldy #50
+	bne invert_key_4
+check_key_4:
+	cmp #27
+	bne check_key_5
+	ldy #122
+	bne invert_key_4
+check_key_5:
+	cmp #19
+	bne check_key_6
+	ldy #126
+	bne invert_key_4
+check_key_6:
+	cmp #23
+	bne check_key_1
+	ldy #130
+	bne invert_key_4
+check_key_1:
+	cmp #25
+	bne check_key_2
+	ldy #202
+	bne invert_key_4
+check_key_2:
+	cmp #29
+	bne check_key_3
+	ldy #206
+	bne invert_key_4
+check_key_3:
+	cmp #21
+	bne check_key_0
+	ldy #210
+invert_key_4:
+	ldx #4
+	bne invert_key
+check_key_0:
+	cmp #17
+	bne check_key_p
+	ldy #26
+	bne invert_key_5
+check_key_p:
+	cmp #18
+	bne check_key_r
+	ldy #30
+	bne invert_key_5
+check_key_r:
+	cmp #15
+	bne no_key_pressed
+	ldy #34
+no_key_pressed:
+	rts
+invert_key_5:
+	ldx #5
+invert_key:
+	sty invert_key_peek + 1
+	sty invert_key_poke + 1
+	sty invert_key_peek2 + 1
+	sty invert_key_poke2 + 1
+	stx invert_key_peek + 2
+	stx invert_key_poke + 2
+	stx invert_key_peek2 + 2
+	stx invert_key_poke2 + 2
+	pha
+invert_key_peek:
+	lda $ffff
+	ora #$80
+invert_key_poke:
+	sta $ffff
+release_key_loop:
+	jsr check_port
+	sta tmp
+	pla
+	cmp tmp
+	bne revert_back
+	pha
+	jmp release_key_loop
+revert_back:
+invert_key_peek2:
+	lda $ffff
+	and #$7f
+invert_key_poke2:
+	sta $ffff
+	rts
+
+check_port:
+	ldx port
+	cpx #1
+	beq read_native_2
+	cpx #16
+	beq read_hummer
+	cpx #17
+	beq read_oem
+	cpx #48
+	beq read_cga_1
+	cpx #49
+	beq read_cga_2
+	cpx #50
+	beq read_pet_1
+	cpx #51
+	beq read_pet_2
+	cpx #52
+	beq read_hit_1
+	cpx #53
+	beq read_hit_2
+	cpx #54
+	beq read_kingsoft_1
+	cpx #55
+	beq read_kingsoft_2
+	cpx #$56
+	beq read_starbyte_1
+	cpx #$57
+	beq read_starbyte_2
+read_native_1:
+	jsr read_native_1_code
+	jmp invert
+read_native_2:
+	jsr read_native_2_code
+	jmp invert
+read_hummer:
+	jsr read_hummer_code
+	jmp invert
+read_oem:
+	jsr read_oem_code
+	jmp invert
+read_cga_1:
+	jsr read_cga_1_code
+	jmp invert
+read_cga_2:
+	jsr read_cga_2_code
+	jmp invert
+read_pet_1:
+	jsr read_pet_1_code
+	jmp invert
+read_pet_2:
+	jsr read_pet_2_code
+	jmp invert
+read_hit_1:
+	jsr read_hit_1_code
+	jmp invert
+read_hit_2:
+	jsr read_hit_2_code
+	jmp invert
+read_kingsoft_1:
+	jsr read_kingsoft_1_code
+	jmp invert
+read_kingsoft_2:
+	jsr read_kingsoft_2_code
+	jmp invert
+read_starbyte_1:
+	jsr read_starbyte_1_code
+	jmp invert
+read_starbyte_2:
+	jsr read_starbyte_2_code
+	jmp invert
+
+read_native_1_code:
+	ldx #$7f
+	stx C64_CIA1_PRA
+	lda C64_CIA1_PRB
+	rts
+
+read_native_2_code:
+	ldy C64_CIA1_DDRA
+	ldx #$ff
+	stx C64_CIA1_DDRA
+	lda C64_CIA1_PRA
+	sty C64_CIA1_DDRA
+	rts
+
+read_hummer_code:
+	ldx #$00
+	stx USERPORT_DDR
+	lda USERPORT_DATA
+	rts
+
+read_oem_code:
+	ldx #$00
+	stx USERPORT_DDR
+	lda USERPORT_DATA
+	tay
+	rol
+	rol
+	and #$01
+	sta tmp
+	tya
+	and #$40
+	lsr
+	lsr
+	lsr
+	lsr
+	lsr
+	ora tmp
+	sta tmp
+	tya
+	and #$20
+	lsr
+	lsr
+	lsr
+	ora tmp
+	sta tmp
+	tya
+	and #$10
+	lsr
+	ora tmp
+	sta tmp
+	tya
+	and #$08
+	asl
+	ora tmp
+	rts
+
+read_cga_1_code:
+	ldx #$80
+	stx USERPORT_DDR
+	stx USERPORT_DATA
+	lda USERPORT_DATA
+	rts
+
+read_cga_2_code:
+	ldx #$80
+	stx USERPORT_DDR
+	ldx #$00
+	stx USERPORT_DATA
+	lda USERPORT_DATA
+	tay
+	and #$0f
+	sta tmp
+	tya
+	and #$20
+	lsr
+	ora tmp
+	rts
+
+read_pet_1_code:
+	ldx #$00
+	stx USERPORT_DDR
+	lda USERPORT_DATA
+	and #$0f
+	cmp #$0c
+	bne notc
+	lda #$0f
+	rts
+notc:
+	ora #$10
+	rts
+
+read_pet_2_code:
+	ldx #$00
+	stx USERPORT_DDR
+	lda USERPORT_DATA
+	lsr
+	lsr
+	lsr
+	lsr
+	cmp #$0c
+	bne notc2
+	lda #$0f
+	rts
+notc2:
+	ora #$10
+	rts
+
+read_hit_1_code:
+	jsr setup_cnt12sp
+	ldy C64_CIA2_PRA
+	ldx C64_CIA2_DDRA
+	lda USERPORT_DATA
+	and #$0f
+	sta tmp
+	txa
+	and #$fb
+	sta C64_CIA2_DDRA
+	lda C64_CIA2_PRA
+	and #$04
+	asl
+	asl
+	ora tmp
+	sty C64_CIA2_PRA
+	stx C64_CIA2_DDRA
+	rts
+
+read_hit_2_code:
+	jsr setup_cnt12sp
+	lda USERPORT_DATA
+	lsr
+	lsr
+	lsr
+	lsr
+	ldx #$0f
+	stx C64_CIA1_SR
+	ldx C64_CIA2_SR
+	beq notsr
+	ora #$10
+notsr:
+	rts
+
+read_kingsoft_1_code:
+	jsr setup_cnt12sp
+	ldx C64_CIA2_PRA
+	ldy C64_CIA2_DDRA
+	lda C64_CIA2_DDRA
+	and #$fb
+	sta C64_CIA2_DDRA
+	lda C64_CIA2_PRA
+	and #$04
+	lsr
+	lsr
+	sta tmp
+	lda USERPORT_DATA
+	and #$80
+	lsr
+	lsr
+	lsr
+	lsr
+	lsr
+	lsr
+	ora tmp
+	sta tmp
+	lda USERPORT_DATA
+	and #$40
+	lsr
+	lsr
+	lsr
+	lsr
+	ora tmp
+	sta tmp
+	lda USERPORT_DATA
+	and #$20
+	lsr
+	lsr
+	ora tmp
+	sta tmp
+	lda USERPORT_DATA
+	and #$10
+	ora tmp
+	stx C64_CIA2_PRA
+	sty C64_CIA2_DDRA
+	rts
+
+read_kingsoft_2_code:
+	jsr setup_cnt12sp
+	lda USERPORT_DATA
+	tax
+	and #$08
+	lsr
+	lsr
+	lsr
+	sta tmp
+	txa
+	and #$04
+	lsr
+	ora tmp
+	sta tmp
+	txa
+	and #$02
+	asl
+	ora tmp
+	sta tmp
+	txa
+	and #$01
+	asl
+	asl
+	asl
+	ora tmp
+	ldx #$ff
+	stx C64_CIA1_SR
+	ldx C64_CIA2_SR
+	beq notsr2
+	ora #$10
+notsr2:
+	rts
+
+read_starbyte_1_code:
+	jsr setup_cnt12sp
+	lda #$00
+	sta tmp
+	ldy C64_CIA2_DDRA
+	ldx #$ff
+	stx C64_CIA1_SR
+	ldx C64_CIA2_SR
+	beq notsr3
+	lda #$10
+	sta tmp
+notsr3:
+	lda USERPORT_DATA
+	tax
+	and #$01
+	asl
+	ora tmp
+	sta tmp
+	txa
+	and #$02
+	asl
+	asl
+	ora tmp
+	sta tmp
+	txa
+	and #$04
+	ora tmp
+	sta tmp
+	txa
+	and #$08
+	lsr
+	lsr
+	lsr
+	ora tmp
+	sty C64_CIA2_DDRA
+	rts
+
+read_starbyte_2_code:
+	jsr setup_cnt12sp
+	ldx C64_CIA2_PRA
+	ldy C64_CIA2_DDRA
+	lda USERPORT_DATA
+	and #$20
+	lsr
+	lsr
+	lsr
+	lsr
+	sta tmp
+	lda USERPORT_DATA
+	and #$40
+	lsr
+	lsr
+	lsr
+	ora tmp
+	sta tmp
+	lda USERPORT_DATA
+	and #$80
+	lsr
+	lsr
+	lsr
+	lsr
+	lsr
+	ora tmp
+	sta tmp
+	lda USERPORT_DATA
+	and #$10
+	ora tmp
+	sta tmp
+	lda C64_CIA2_DDRA
+	and #$fb
+	sta C64_CIA2_DDRA
+	lda C64_CIA2_PRA
+	and #$04
+	lsr
+	lsr
+	ora tmp
+	sty C64_CIA2_PRA
+	stx C64_CIA2_DDRA
+	rts
+
+invert:
+	and #$1f
+	rts
+
+setup_cnt12sp:
+	ldx #$00
+	stx USERPORT_DDR
+	inx
+	stx C64_CIA2_TIMER_A_LOW
+	dex
+	stx C64_CIA2_TIMER_A_HIGH
+	ldx #$11
+	stx C64_CIA2_CRA
+	ldx #$01
+	stx C64_CIA1_TIMER_A_LOW
+	dex
+	stx C64_CIA1_TIMER_A_HIGH
+	ldx #$51
+	stx C64_CIA1_CRA
+	rts
+
+check_f1:
+	lda $c6		; keys in buffer
+	beq no_key
+	lda $0277		; 1st key
+	cmp #133		; F1
+	beq change_port
+	lda #$00
+	sta $c6		; keys in buffer
+no_key:
+	rts
+
+change_port:
+	ldx #$00
+print_change_port_screen_loop1:
+	lda change_port_screen_top,x
+	beq print_change_port_screen2
+	jsr $ffd2
+	inx
+	bne print_change_port_screen_loop1
+print_change_port_screen2:
+	ldx #$00
+print_change_port_screen_loop2:
+	lda change_port_screen_bottom,x
+	beq check_change_port_key
+	jsr $ffd2
+	inx
+	bne print_change_port_screen_loop2
+check_change_port_key:
+	ldx #$00
+	stx $c6
+port_key_loop:
+	ldx $c6
+	beq port_key_loop
+	ldx $0277
+	cpx #'1'
+	bne check2
+	ldx #0
+	bne new_port
+check2:
+	cpx #'2'
+	bne check3
+	ldx #1
+	bne new_port
+check3:
+	cpx #'3'
+	bne check4
+	ldx #48
+	bne new_port
+check4:
+	cpx #'4'
+	bne check5
+	ldx #49
+	bne new_port
+check5:
+	cpx #'5'
+	bne check6
+	ldx #50
+	bne new_port
+check6:
+	cpx #'6'
+	bne check7
+	ldx #51
+	bne new_port
+check7:
+	cpx #'7'
+	bne check8
+	ldx #16
+	bne new_port
+check8:
+	cpx #'8'
+	bne check9
+	ldx #17
+	bne new_port
+check9:
+	cpx #'9'
+	bne checkA
+	ldx #52
+	bne new_port
+checkA:
+	cpx #'A'
+	bne checkB
+	ldx #53
+	bne new_port
+checkB:
+	cpx #'B'
+	bne checkC
+	ldx #54
+	bne new_port
+checkC:
+	cpx #'C'
+	bne checkD
+	ldx #55
+	bne new_port
+checkD:
+	cpx #'D'
+	bne checkE
+	ldx #56
+	bne new_port
+checkE:
+	cpx #'E'
+	bne check_change_port_key
+	ldx #57
+new_port:
+	stx port
+	jmp mainloop
+
+print_press_f1_screen:
+	ldx #$00
+press_f1_screen_loop:
+	lda press_f1_screen,x
+	beq end_f1_screen_loop
+	jsr $ffd2
+	inx
+	bne press_f1_screen_loop
+end_f1_screen_loop:
+	rts
+
+print_main_screen:
+	ldx #$00
+screen_loop:
+	lda main_screen,x
+	beq end_loop
+	jsr $ffd2
+	inx
+	bne screen_loop
+end_loop:
+	rts
+
+print_test_name_screen:
+	ldx #$00
+test_name_screen_loop:
+	lda test_name_screen,x
+	beq end_loop
+	jsr $ffd2
+	inx
+	bne test_name_screen_loop
+
+print_joy_device_screen:
+	ldx #$00
+	lda port
+	and #$10
+	beq print_native_device_loop
+	lda port
+print_userport_device_loop:
+	lda userport_device_screen,x
+	beq print_userport_type
+	jsr $ffd2
+	inx
+	bne print_userport_device_loop
+print_native_device_loop:
+	lda native_device_screen,x
+	beq print_port_number
+	jsr $ffd2
+	inx
+	bne print_native_device_loop
+print_userport_type:
+	lda #$20
+	jsr $ffd2
+	lda port
+	cmp #16
+	beq is_hummer_device
+	cmp #17
+	beq is_oem_device
+	and #$fe
+	cmp #48
+	beq is_cga_device
+	cmp #50
+	beq is_pet_device
+	cmp #52
+	beq is_hit_device
+	cmp #54
+	beq is_kingsoft_device
+	ldy #<starbyte_screen
+	ldx #>starbyte_screen
+	jmp print_type
+is_hummer_device:
+	ldy #<hummer_screen
+	ldx #>hummer_screen
+	jmp print_type
+is_oem_device:
+	ldy #<oem_screen
+	ldx #>oem_screen
+	jmp print_type
+is_cga_device:
+	ldy #<cga_screen
+	ldx #>cga_screen
+	jmp print_type
+is_pet_device:
+	ldy #<pet_screen
+	ldx #>pet_screen
+	jmp print_type
+is_hit_device:
+	ldy #<hit_screen
+	ldx #>hit_screen
+	jmp print_type
+is_kingsoft_device:
+	ldy #<kingsoft_screen
+	ldx #>kingsoft_screen
+print_type:
+	sty print_type_loop + 1
+	stx print_type_loop + 2
+	ldx #$00
+print_type_loop:
+	lda $ffff,x
+	beq end_print_type_loop
+	jsr $ffd2
+	inx
+	bne print_type_loop
+end_print_type_loop:
+	lda port
+	and #$20
+	beq end_device_print
+print_port_number:
+	lda #$20
+	jsr $ffd2
+	lda port
+	and #$01
+	clc
+	adc #'1'
+	jsr $ffd2
+end_device_print:
+	lda #13
+	jmp $ffd2
+
+port:	!by 0
+
+tmp:	!by 0
+
+main_screen:
+	!by 147
+	!by 176,195,195,195,178,195,195,195,178,195,195,195,174,13
+	!by 194, 32, 55, 32,194, 32, 56, 32,194, 32, 57, 32,194,13
+	!by 171,195,195,195,219,195,195,195,219,195,195,195,179,13
+	!by 194, 32, 52, 32,194, 32, 53, 32,194, 32, 54, 32,194,13
+	!by 171,195,195,195,219,195,195,195,219,195,195,195,179,13
+	!by 194, 32, 49, 32,194, 32, 50, 32,194, 32, 51, 32,194,13
+	!by 171,195,195,195,219,195,195,195,219,195,195,195,179,13
+	!by 194, 32, 48, 32,194, 32, 80, 32,194, 32, 82, 32,194,13
+	!by 173,195,195,195,177,195,195,195,177,195,195,195,189,13
+	!by 0
+
+test_name_screen:
+	!by 13,'C','O','P','L','I','N',' ','K','E','Y','P','A','D',' ','I','N',13,0
+
+userport_device_screen:
+	!by 'U','S','E','R','P','O','R','T',0
+
+starbyte_screen:
+	!by 'S','T','A','R','B','Y','T','E',0
+
+hummer_screen:
+	!by 'H','U','M','M','E','R',0
+
+oem_screen:
+	!by 'O','E','M',0
+
+cga_screen:
+	!by 'C','G','A',0
+
+pet_screen:
+	!by 'P','E','T',0
+
+hit_screen:
+	!by 'H','I','T',0
+
+kingsoft_screen:
+	!by 'K','I','N','G','S','O','F','T',0
+
+native_device_screen:
+	!by 'N','A','T','I','V','E',0
+
+press_f1_screen:
+	!by 13,'P','R','E','S','S',' ','F','1',' ','T','O',' ','C','H','A','N','G','E',' ','P','O','R','T',13,0
+
+change_port_screen_top:
+	!by 147,'P','L','E','A','S','E',' ','C','H','O','O','S','E',' ','T','H','E',' ','N','E','W',' ','P','O','R','T',':',13
+	!by '1',')',' ','N','A','T','I','V','E',' ','1',13
+	!by '2',')',' ','N','A','T','I','V','E',' ','2',13
+	!by '3',')',' ','U','S','E','R','P','O','R','T',' ','C','G','A',' ','1',13
+	!by '4',')',' ','U','S','E','R','P','O','R','T',' ','C','G','A',' ','2',13
+	!by '5',')',' ','U','S','E','R','P','O','R','T',' ','P','E','T',' ','1',13
+	!by '6',')',' ','U','S','E','R','P','O','R','T',' ','P','E','T',' ','2',13
+	!by '7',')',' ','U','S','E','R','P','O','R','T',' ','H','U','M','M','E','R',13
+	!by '8',')',' ','U','S','E','R','P','O','R','T',' ','O','E','M',13,0
+change_port_screen_bottom:
+	!by '9',')',' ','U','S','E','R','P','O','R','T',' ','H','I','T',' ','1',13
+	!by 'A',')',' ','U','S','E','R','P','O','R','T',' ','H','I','T',' ','2',13
+	!by 'B',')',' ','U','S','E','R','P','O','R','T',' ','K','I','N','G','S','O','F','T',' ','1',13
+	!by 'C',')',' ','U','S','E','R','P','O','R','T',' ','K','I','N','G','S','O','F','T',' ','2',13
+	!by 'D',')',' ','U','S','E','R','P','O','R','T',' ','S','T','A','R','B','Y','T','E',' ','1',13
+	!by 'E',')',' ','U','S','E','R','P','O','R','T',' ','S','T','A','R','B','Y','T','E',' ','2',13,0
