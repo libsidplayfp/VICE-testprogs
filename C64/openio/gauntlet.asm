52a6fa9a97c00b45b159f6f8de91a9c911cb3e57
groepaz <gpz@users.sourceforge.net>
2018-05-21 17:02:31 +0000

    updated test
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/testprogs@34883 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- C64/openio/gauntlet.asm
+++ C64/openio/gauntlet.asm
@@ -13,6 +13,35 @@ start:
     lda #$35
     sta $01
 
+    ldx #0
+-
+    txa
+    sta $0400,x
+    sta $0500,x
+    sta $0600,x
+    sta $0700,x
+    inx
+    bne -
+
+    jsr test
+
+    ; passed
+    jsr clear
+
+    ldx #7
+-
+    lda txtpassed,x
+    sta $0400,x
+    dex
+    bpl -
+
+    lda #5
+    sta $d020
+    lda #$00
+    sta $d7ff
+    jmp *
+
+test:
     LDY #$1E
 i4067:
     JSR i406D      ; run the check 30 times
@@ -79,12 +108,7 @@ i40CF:
     BEQ failed
     STA $8004
 
-    ; passed
-    lda #5
-    sta $d020
-    lda #$00
-    sta $d7ff
-    jmp *
+    rts
 
     ; failed
 failed:
@@ -92,11 +116,32 @@ failed:
     sta $d020
     lda #$ff
     sta $d7ff
+    jsr clear
+    ldx #7
+-
+    lda txtfailed,x
+    sta $0400,x
+    dex
+    bpl -
     jmp *
 
+clear:
+    lda #$20
+    ldx #0
+-
+    sta $0400,x
+    sta $0500,x
+    sta $0600,x
+    sta $0700,x
+    inx
+    bne -
+    rts
 
 i4178:  !byte $5f, $df, $c7, $20, $53, $91, $74, $f0, $6d, $ab, $45, $1a, $64, $72, $73, $2e, $ff 
 i4189:  !byte $0f, $96
 i418B:  !byte $01, $02, $03, $04, $05, $06, $07, $08, $09, $10, $11, $12, $13, $14, $15, $16, $96, $ff
 i419D:  !byte $0f, $96
 i419F:  !byte $01, $02, $03, $04, $05, $06, $07, $08, $09, $10, $11, $12, $13, $14, $15, $16, $96, $00
+
+txtpassed: !scr "passed  "
+txtfailed: !scr "failed  "
