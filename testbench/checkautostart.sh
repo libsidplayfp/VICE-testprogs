b6b096c7460f3a9283abd3e275c6560ae4f6fc03
groepaz <gpz@users.sourceforge.net>
2020-01-09 03:06:53 +0000

    reordered tests, added some comments
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/testprogs@37341 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- testbench/checkautostart.sh
+++ testbench/checkautostart.sh
@@ -12,7 +12,7 @@ function dotest
 
 checkopts="$3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13}"
 
-#echo $VICEDIR/$1 -debugcart -console -warp -limitcycles $LIMITCYCLES $checkopts $2
+#echo $VICEDIR/$1 $checkopts -debugcart -console -warp -limitcycles $LIMITCYCLES $2
 
 echo -ne $1" "$checkopts" # "
 
@@ -49,55 +49,56 @@ function alltests
 {
 echo $EMU":"$PROG
 dotest $EMU $PROG -default $OPTS
-dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 0 -autostart-handle-tde -autostart-warp
-dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 0 -autostart-handle-tde +autostart-warp
-dotest $EMU $PROG $OPTS -truedrive +virtualdev -autostartprgmode 0 -autostart-handle-tde -autostart-warp
-dotest $EMU $PROG $OPTS -truedrive +virtualdev -autostartprgmode 0 -autostart-handle-tde +autostart-warp
-dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 0 -autostart-handle-tde -autostart-warp
-dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 0 -autostart-handle-tde +autostart-warp
-dotest $EMU $PROG $OPTS -truedrive -virtualdev -autostartprgmode 0 -autostart-handle-tde -autostart-warp
-dotest $EMU $PROG $OPTS -truedrive -virtualdev -autostartprgmode 0 -autostart-handle-tde +autostart-warp
-dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 1 -autostart-handle-tde -autostart-warp
-dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 1 -autostart-handle-tde +autostart-warp
-dotest $EMU $PROG $OPTS -truedrive +virtualdev -autostartprgmode 1 -autostart-handle-tde -autostart-warp
-dotest $EMU $PROG $OPTS -truedrive +virtualdev -autostartprgmode 1 -autostart-handle-tde +autostart-warp
-dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 1 -autostart-handle-tde -autostart-warp
-dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 1 -autostart-handle-tde +autostart-warp
-dotest $EMU $PROG $OPTS -truedrive -virtualdev -autostartprgmode 1 -autostart-handle-tde -autostart-warp
-dotest $EMU $PROG $OPTS -truedrive -virtualdev -autostartprgmode 1 -autostart-handle-tde +autostart-warp
-dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 2 -autostart-handle-tde -autostart-warp
-dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 2 -autostart-handle-tde +autostart-warp
-dotest $EMU $PROG $OPTS -truedrive +virtualdev -autostartprgmode 2 -autostart-handle-tde -autostart-warp
-dotest $EMU $PROG $OPTS -truedrive +virtualdev -autostartprgmode 2 -autostart-handle-tde +autostart-warp
-dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 2 -autostart-handle-tde -autostart-warp
-dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 2 -autostart-handle-tde +autostart-warp
-dotest $EMU $PROG $OPTS -truedrive -virtualdev -autostartprgmode 2 -autostart-handle-tde -autostart-warp
-dotest $EMU $PROG $OPTS -truedrive -virtualdev -autostartprgmode 2 -autostart-handle-tde +autostart-warp
 
-dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 0 +autostart-handle-tde -autostart-warp
-dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 0 +autostart-handle-tde +autostart-warp
 dotest $EMU $PROG $OPTS -truedrive +virtualdev -autostartprgmode 0 +autostart-handle-tde -autostart-warp
 dotest $EMU $PROG $OPTS -truedrive +virtualdev -autostartprgmode 0 +autostart-handle-tde +autostart-warp
-dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 0 +autostart-handle-tde -autostart-warp
-dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 0 +autostart-handle-tde +autostart-warp
 dotest $EMU $PROG $OPTS -truedrive -virtualdev -autostartprgmode 0 +autostart-handle-tde -autostart-warp
 dotest $EMU $PROG $OPTS -truedrive -virtualdev -autostartprgmode 0 +autostart-handle-tde +autostart-warp
-dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 1 +autostart-handle-tde -autostart-warp
-dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 1 +autostart-handle-tde +autostart-warp
+dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 0 +autostart-handle-tde -autostart-warp
+dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 0 +autostart-handle-tde +autostart-warp
+dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 0 +autostart-handle-tde -autostart-warp
+dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 0 +autostart-handle-tde +autostart-warp
 dotest $EMU $PROG $OPTS -truedrive +virtualdev -autostartprgmode 1 +autostart-handle-tde -autostart-warp
 dotest $EMU $PROG $OPTS -truedrive +virtualdev -autostartprgmode 1 +autostart-handle-tde +autostart-warp
-dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 1 +autostart-handle-tde -autostart-warp
-dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 1 +autostart-handle-tde +autostart-warp
 dotest $EMU $PROG $OPTS -truedrive -virtualdev -autostartprgmode 1 +autostart-handle-tde -autostart-warp
 dotest $EMU $PROG $OPTS -truedrive -virtualdev -autostartprgmode 1 +autostart-handle-tde +autostart-warp
-dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 2 +autostart-handle-tde -autostart-warp
-dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 2 +autostart-handle-tde +autostart-warp
+dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 1 +autostart-handle-tde -autostart-warp
+dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 1 +autostart-handle-tde +autostart-warp
+dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 1 +autostart-handle-tde -autostart-warp
+dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 1 +autostart-handle-tde +autostart-warp
 dotest $EMU $PROG $OPTS -truedrive +virtualdev -autostartprgmode 2 +autostart-handle-tde -autostart-warp
 dotest $EMU $PROG $OPTS -truedrive +virtualdev -autostartprgmode 2 +autostart-handle-tde +autostart-warp
-dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 2 +autostart-handle-tde -autostart-warp
-dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 2 +autostart-handle-tde +autostart-warp
 dotest $EMU $PROG $OPTS -truedrive -virtualdev -autostartprgmode 2 +autostart-handle-tde -autostart-warp
 dotest $EMU $PROG $OPTS -truedrive -virtualdev -autostartprgmode 2 +autostart-handle-tde +autostart-warp
+dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 2 +autostart-handle-tde -autostart-warp
+dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 2 +autostart-handle-tde +autostart-warp
+dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 2 +autostart-handle-tde -autostart-warp
+dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 2 +autostart-handle-tde +autostart-warp
+
+dotest $EMU $PROG $OPTS -truedrive +virtualdev -autostartprgmode 0 -autostart-handle-tde -autostart-warp
+dotest $EMU $PROG $OPTS -truedrive +virtualdev -autostartprgmode 0 -autostart-handle-tde +autostart-warp
+dotest $EMU $PROG $OPTS -truedrive -virtualdev -autostartprgmode 0 -autostart-handle-tde -autostart-warp
+dotest $EMU $PROG $OPTS -truedrive -virtualdev -autostartprgmode 0 -autostart-handle-tde +autostart-warp
+dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 0 -autostart-handle-tde -autostart-warp
+dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 0 -autostart-handle-tde +autostart-warp
+dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 0 -autostart-handle-tde -autostart-warp
+dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 0 -autostart-handle-tde +autostart-warp
+dotest $EMU $PROG $OPTS -truedrive +virtualdev -autostartprgmode 1 -autostart-handle-tde -autostart-warp
+dotest $EMU $PROG $OPTS -truedrive +virtualdev -autostartprgmode 1 -autostart-handle-tde +autostart-warp
+dotest $EMU $PROG $OPTS -truedrive -virtualdev -autostartprgmode 1 -autostart-handle-tde -autostart-warp
+dotest $EMU $PROG $OPTS -truedrive -virtualdev -autostartprgmode 1 -autostart-handle-tde +autostart-warp
+dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 1 -autostart-handle-tde -autostart-warp
+dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 1 -autostart-handle-tde +autostart-warp
+dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 1 -autostart-handle-tde -autostart-warp
+dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 1 -autostart-handle-tde +autostart-warp
+dotest $EMU $PROG $OPTS -truedrive +virtualdev -autostartprgmode 2 -autostart-handle-tde -autostart-warp
+dotest $EMU $PROG $OPTS -truedrive +virtualdev -autostartprgmode 2 -autostart-handle-tde +autostart-warp
+dotest $EMU $PROG $OPTS -truedrive -virtualdev -autostartprgmode 2 -autostart-handle-tde -autostart-warp
+dotest $EMU $PROG $OPTS -truedrive -virtualdev -autostartprgmode 2 -autostart-handle-tde +autostart-warp
+dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 2 -autostart-handle-tde -autostart-warp
+dotest $EMU $PROG $OPTS +truedrive +virtualdev -autostartprgmode 2 -autostart-handle-tde +autostart-warp
+dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 2 -autostart-handle-tde -autostart-warp
+dotest $EMU $PROG $OPTS +truedrive -virtualdev -autostartprgmode 2 -autostart-handle-tde +autostart-warp
 echo "---"
 }
 
@@ -107,8 +108,10 @@ function testx64
 LIMITCYCLES=10000000
 EMU=x64
 OPTS=
+echo "the first four tests fail as expected. TDE enabled, handle TDE disabled and autostartmode 0 can not work."
 PROG=./selftest/c64-pass.prg
 alltests
+echo "all tests pass"
 PROG=./selftest/c64-pass.d64
 alltests
 }
@@ -119,8 +122,10 @@ function testx64sc
 LIMITCYCLES=10000000
 EMU=x64sc
 OPTS=
+echo "the first four tests fail as expected. TDE enabled, handle TDE disabled and autostartmode 0 can not work."
 PROG=./selftest/c64-pass.prg
 alltests
+echo "all tests pass"
 PROG=./selftest/c64-pass.d64
 alltests
 }
@@ -131,24 +136,30 @@ function testx128
 LIMITCYCLES=10000000
 EMU=x128
 OPTS=-go64
+echo "the first four tests fail as expected. TDE enabled, handle TDE disabled and autostartmode 0 can not work."
 PROG=./selftest/c64-pass.prg
 alltests
+echo "all tests pass"
 PROG=./selftest/c64-pass.d64
 alltests
 
 # x128 (VIC)
 EMU=x128
 OPTS=-40col
+echo "the first four tests fail as expected. TDE enabled, handle TDE disabled and autostartmode 0 can not work."
 PROG=./selftest/c128-pass.prg
 alltests
+echo "all tests pass"
 PROG=./selftest/c128-pass.d64
 alltests
 
 # x128 (VDC)
 EMU=x128
 OPTS=-80col
+echo "the first four tests fail as expected. TDE enabled, handle TDE disabled and autostartmode 0 can not work."
 PROG=./selftest/c128-pass.prg
 alltests
+echo "all tests pass"
 PROG=./selftest/c128-pass.d64
 alltests
 }
@@ -159,8 +170,10 @@ function testxvic
 LIMITCYCLES=10000000
 EMU=xvic
 OPTS="-memory 8k"
+echo "the first four tests fail as expected. TDE enabled, handle TDE disabled and autostartmode 0 can not work."
 PROG=./selftest/vic20-pass.prg
 alltests
+echo "all tests pass"
 PROG=./selftest/vic20-pass.d64
 alltests
 }
@@ -171,8 +184,10 @@ function testx64dtv
 LIMITCYCLES=50000000
 EMU=x64dtv
 OPTS=
+echo "all tests pass"
 PROG=./selftest/dtv-pass.prg
 alltests
+echo "all tests pass"
 PROG=./selftest/dtv-pass.d64
 alltests
 }
@@ -180,11 +195,13 @@ alltests
 # xplus4
 function testxplus4
 {
-LIMITCYCLES=10000000
+LIMITCYCLES=15000000
 EMU=xplus4
 OPTS=
+echo "the first four tests fail as expected. TDE enabled, handle TDE disabled and autostartmode 0 can not work."
 PROG=./selftest/plus4-pass.prg
 alltests
+echo "FIXME: all tests pass, except sometimes some fail randomly."
 PROG=./selftest/plus4-pass.d64
 alltests
 }
@@ -195,8 +212,11 @@ function testxpet
 LIMITCYCLES=20000000
 EMU=xpet
 OPTS="-drive8type 8250"
+echo "the first four tests fail as expected. TDE enabled, handle TDE disabled and autostartmode 0 can not work."
+echo "FIXME: there is too much failing here"
 PROG=./selftest/pet-pass.prg
 alltests
+echo "all tests pass"
 PROG=./selftest/pet-pass.d82
 alltests
 }
@@ -207,8 +227,10 @@ function testxscpu64
 LIMITCYCLES=10000000
 EMU=xscpu64
 OPTS=
+echo "the first four tests fail as expected. TDE enabled, handle TDE disabled and autostartmode 0 can not work."
 PROG=./selftest/scpu-pass.prg
 alltests
+echo "all tests pass"
 PROG=./selftest/scpu-pass.d64
 alltests
 }
@@ -219,8 +241,11 @@ function testxcbm2
 LIMITCYCLES=80000000
 EMU=xcbm2
 OPTS="-drive8type 8250"
+echo "the first four tests fail as expected. TDE enabled, handle TDE disabled and autostartmode 0 can not work."
+echo "FIXME: there is too much failing here"
 PROG=./selftest/cbm610-pass.prg
 alltests
+echo "all tests pass"
 PROG=./selftest/cbm610-pass.d82
 alltests
 }
@@ -231,19 +256,23 @@ function testxcbm5x0
 LIMITCYCLES=80000000
 EMU=xcbm5x0
 OPTS="-drive8type 8250"
+echo "the first four tests fail as expected. TDE enabled, handle TDE disabled and autostartmode 0 can not work."
+echo "FIXME: there is too much failing here"
 PROG=./selftest/cbm510-pass.prg
 alltests
+echo "all tests pass"
 PROG=./selftest/cbm510-pass.d82
 alltests
 }
 
-testx128
-testx64sc
-testx64
-testxvic
-testxplus4
-testx64dtv
-testxscpu64
+
 testxpet
 testxcbm2
 testxcbm5x0
+testxplus4
+testxscpu64
+testxvic
+testx128
+testx64sc
+testx64
+testx64dtv
