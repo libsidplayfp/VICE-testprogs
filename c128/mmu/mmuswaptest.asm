d8bc9fb9bb9fc515d411fdcea91e9744544e4e60
Marco van den Heuvel <blackystardust@users.sourceforge.net>
2021-12-31 05:31:12 +0000

    Extended the mmu swap test to also test swapping of the stack page.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/testprogs@41450 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- c128/mmu/mmuswaptest.asm
+++ c128/mmu/mmuswaptest.asm
@@ -18,20 +18,35 @@ basicHeader=1
 
 	sei
 	ldy $d507
+	ldx $d509
 	lda #$aa
 	sta $80   ; store in real zero page
 	lda #$55
 	sta $3080 ; store in 'soon to become' zero page
 	lda #$30
-	sta $d507 ; relocate zero page to $38xx
+	sta $d507 ; relocate zero page to $30xx
 	lda $80
 	cmp #$55  ; expecting $55
 	bne failed
 	lda $3080
 	cmp #$aa  ; expecting $aa
+	sty $d507
+	lda #$aa
+	sta $0120 ; store in the real stack page
+	lda #$55
+	sta $3120 ; store in 'soon to become' stack page
+	lda #$31
+	sta $d509 ; relocate stack page to $31xx
+	lda $0120
+	cmp #$55  ; expecting $55
+	bne failed
+	lda $3120
+	cmp #$aa  ; expecting $aa
 	bne failed
 
 passed:
+	sty $d507
+	stx $d509
 	ldx #0	
 -	
 	lda ok_msg,x
@@ -44,10 +59,11 @@ passed:
 	sta $d020
 	lda #$00
 	sta $d7ff
-	sty $d507
 	jmp *
 
 failed:
+	sty $d507
+	stx $d509
 	ldx #0	
 -	
 	lda error_msg,x
@@ -60,7 +76,6 @@ failed:
 	sta $d020
 	lda #$ff
 	sta $d7ff
-	sty $d507
 	jmp *	
 
 error_msg:
