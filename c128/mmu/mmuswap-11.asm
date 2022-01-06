b91bc5ae3c353f84858ee960493e25e52567cffd
Marco van den Heuvel <blackystardust@users.sourceforge.net>
2022-01-06 02:49:23 +0000

    Changed test 11 to passed green based on c128 hardware test results.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/testprogs@41515 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- c128/mmu/mmuswap-11.asm
+++ c128/mmu/mmuswap-11.asm
@@ -56,7 +56,7 @@ basicHeader=1
 	bne failed
 zp_bank0_55:
 	cpy #$55
-	beq zp_bank0_55_bank1_55
+	beq passed
 	cpy #$aa
 	beq zp_bank0_55_bank1_aa
 	ldx #10
@@ -68,9 +68,6 @@ zp_bank0_aa:
 	beq zp_bank0_aa_bank1_aa
 	ldx #10
 	bne failed
-zp_bank0_55_bank1_55:
-	ldx #0
-	beq failed
 zp_bank0_55_bank1_aa:
 	ldx #1
 	bne failed
