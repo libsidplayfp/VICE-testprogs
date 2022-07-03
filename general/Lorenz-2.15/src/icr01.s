622cde8eb4a0d9887ef83bd7db1753b8caeba842
groepaz <gpz@users.sourceforge.net>
2022-07-03 15:10:49 +0000

    fix some of the half complete refactoring, which made some tests crash when loaded by the previous one. fixes bug #1754
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/testprogs@42489 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- general/Lorenz-2.15/src/icr01.s
+++ general/Lorenz-2.15/src/icr01.s
@@ -1,180 +1,38 @@
+; this file is part of the C64 Emulator Test Suite. public domain, no copyright
+
 ; original file was: icr01.asm
 ;-------------------------------------------------------------------------------
 
-;icr01.asm - this file is part
-;of the C64 Emulator Test Suite
-;public domain, no copyright
+            .include "common.asm"
+            .include "printhb.asm"
+            .include "waitkey.asm"
+            .include "waitborder.asm"
 
-           *= $0801
-           .byte $4c,$14,$08,$00,$97
-turboass   = 780
-           .text "780"
-           .byte $2c,$30,$3a,$9e,$32,$30
-           .byte $37,$33,$00,$00,$00
-           .block
-           lda #1
-           sta turboass
-           ldx #0
-           stx $d3
-           lda thisname
-printthis
-           jsr $ffd2
-           inx
-           lda thisname,x
-           bne printthis
+;------------------------------------------------------------------------------
+thisname   .null "icr01"     ; name of this test
+nextname   .null "imr"       ; name of next test, "-" means no more tests
+;------------------------------------------------------------------------------
+main:
+
+; FIXME: save/restore vectors shouldnt be necessary here
            sec
            jsr vector
-           jsr main
-           lda #$37
-           sta 1
-           lda #$2f
-           sta 0
+           jsr oldmain
            clc
            jsr vector
-           jsr $fda3
-           jsr print
-           .text " - ok"
-           .byte 13,0
-            lda #$00 ; success
-            sta $d7ff
-           .bend
-loadnext
-           .block
-           ldx #$f8
-           txs
-           lda nextname
-           cmp #"-"
-           bne notempty
-           jmp $a474
-notempty
-           ldx #0
-printnext
-           jsr $ffd2
-           inx
-           lda nextname,x
-           bne printnext
-           lda #0
-           sta $0a
-           sta $b9
-           stx $b7
-           lda #<nextname
-           sta $bb
-           lda #>nextname
-           sta $bc
-           jmp $e16f
-           .bend
 
-;---------------------------------------
-;print text which immediately follows
-;the JSR and return to address after 0
-
-print
-           .block
-           pla
-           sta next+1
-           pla
-           sta next+2
-           ldx #1
-next
-           lda $1111,x
-           beq end
-           jsr $ffd2
-           inx
-           bne next
-end
-           sec
-           txa
-           adc next+1
-           sta return+1
-           lda #0
-           adc next+2
-           sta return+2
-return
-           jmp $1111
-           .bend
-
-;---------------------------------------
-;print hex byte
-
-printhb
-           .block
-           pha
-           lsr a
-           lsr a
-           lsr a
-           lsr a
-           jsr printhn
-           pla
-           and #$0f
-printhn
-           ora #$30
-           cmp #$3a
-           bcc noletter
-           adc #6
-noletter
-           jmp $ffd2
-           .bend
-
-;---------------------------------------
-;wait until raster line is in border
-;to prevent getting disturbed by DMAs
-
-waitborder
-           .block
-;           lda $d011
-;           bmi ok
-;wait
-;           lda $d012
-;           cmp #30
-;           bcs wait
-;ok
-wait
-           lda $d011
-           bpl wait
-           rts
-           .bend
-
-;---------------------------------------
-;wait for a key and check for STOP
-
-waitkey
-            lda #$ff ; failure
-            sta $d7ff
-
-           .block
-           clc
-           jsr vector
-           jsr $fda3
-           cli
-wait
-           jsr $ffe4
-           beq wait
-           cmp #3
-           beq stop
-           rts
-stop
-           lda turboass
-           beq load
-           jmp $8000
-load
-           jsr print
-           .byte 13
-           .text "break"
-           .byte 13,0
-           jmp loadnext
-           .bend
+           rts  ; SUCCESS
 
 ;---------------------------------------
 ; vector save/restore
 
-vector     ldx #<$334
-           ldy #>$334
-           jmp $ff8d
-
-;---------------------------------------
-
-thisname   .null "icr01"
-nextname   .null "imr"
+; FIXME: shouldnt complete vector reset be good enough?
+; used before/after main, and in waitkey
+vector     
+;           ldx #<$334
+;           ldy #>$334
+;           jmp $ff8d
+            rts
 
 ;---------------------------------------
 
@@ -194,7 +52,7 @@ onnmi
            pla
            rti
 
-main
+oldmain:
 
 ;---------------------------------------
 ;read icr when it is $01 and check if
