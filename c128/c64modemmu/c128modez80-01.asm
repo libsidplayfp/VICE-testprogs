7079d60e8d3cce5aec6eeb1161400cf0ce530947
Marco van den Heuvel <blackystardust@users.sourceforge.net>
2023-02-25 04:19:26 +0000

    Added a c128 mode test to see if the z80 can access the I/O by in/out AND memory access.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/testprogs@43294 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- c128/c64modemmu/c128modez80-01.asm
+++ c128/c64modemmu/c128modez80-01.asm
@@ -1,12 +1,12 @@
-; This is a c128 mode test to see if the z80 can access the c128 mode roms.
+; This is a c128 mode test to see if the z80 can access the I/O by in/out AND memory access.
 ;
 ; test to be confirmed on real hardware
 ;
 ; colors:
 ;   black  = was not able to switch on the z80
 ;   white  = got z80 switched on, but no z80 bios present
-;   cyan   = z80 on, z80 bios present in c128 mode, c128 mode roms accessable
-;   violet = z80 on, z80 bios present in c128 mode, c128 mode roms NOT accessable
+;   cyan   = z80 on, z80 bios present in c128 mode, I/O only accessable through in/out
+;   violet = z80 on, z80 bios present in c128 mode, I/O accessable through both in/out and memory accesses
 ;
 ; Test made by Marco van den Heuvel
 
@@ -33,7 +33,7 @@ basicHeader=1
 	lda #$00
 	sta $d506
 
-; bank in bank 0 and make everything rom
+; bank in bank 0 and bank in I/O
 	lda #$00
 	sta $ff00
 
@@ -46,4 +46,4 @@ basicHeader=1
 
 *=$2000
  
-!binary "c128modez80-00.bin"
+!binary "c128modez80-01.bin"
---
+++ c128/c64modemmu/c128modez80-01.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+
+void main()
+{
+
+#asm
+	/* set the border color to indicate we made it into z80 mode */
+	ld a,1
+	ld bc,0xd020
+	out (c),a
+
+	/* check if we can access the z80 bios */
+	ld bc,0x048d
+	ld a,(bc)
+	cp 0x43
+	jr nz,noz80bios
+
+	inc bc
+	ld a,(bc)
+	cp 0x50
+	jr nz,noz80bios
+
+	inc bc
+	ld a,(bc)
+	cp 0x4d
+	jr z,z80bios_found
+
+noz80bios:
+	jr nz,noz80bios
+
+z80bios_found:
+	/* set the border color to indicate we found the z80 bios in c128 mode */
+	ld a,3
+	ld bc,0xd020
+	out (c),a
+
+	/* try to set the border color through memory access */
+	ld a,4
+	ld (bc),a
+
+	ld a,1
+	cp 1
+justloop:
+	jr z,justloop
+
+#endasm
+}
