e6145e9c6860732a786fa405d84d0e06c794cf3f
Marco van den Heuvel <blackystardust@users.sourceforge.net>
2023-02-26 19:07:09 +0000

    Added a c64 mode test to see which vicii color map the z80 can access in c64 mode when the current vicii color map bank has been set to bank 0 in c128 mode.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/testprogs@43316 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ c128/c64modemmu/c64modez80-04.asm
@@ -0,0 +1,88 @@
+; This is a c64 mode test to see which vicii color map the z80 can access in c64 mode when the current vicii color map bank has been set to bank 0 in c128 mode.
+;
+; test confirmed on real hardware
+;
+; colors:
+;   black  = was not able to switch on the z80
+;   white  = got z80 switched on, but no z80 bios present
+;   cyan   = z80 on, z80 bios present in c128 mode, but could not get into c64 mode
+;   violet = z80 on, z80 bios present in c128 mode, got to c64 mode, no access to vicii color map at all
+;   blue   = z80 on, z80 bios present in c128 mode, got to c64 mode, access to vicii color map bank 0
+;   yellow = z80 on, z80 bios present in c128 mode, got to c64 mode, access to vicii color map bank 1
+;
+; Test made by Marco van den Heuvel
+
+
+start=$1c40
+
+basicHeader=1 
+
+!ifdef basicHeader {
+; 10 SYS7181
+*=$1c01
+	!byte  $0c,$08,$0a,$00,$9e,$37,$31,$38,$31,$00,$00,$00
+*=$1c0d 
+	jmp start
+}
+*=start
+; clear the screen
+	lda #$93
+	jsr $ffd2
+
+	sei
+
+; no shared memory
+	lda #$00
+	sta $d506
+
+; bank in bank 0 and make everything ram, bank in I/O
+	lda #$3e
+	sta $ff00
+
+; change the border color to black
+	lda #$00
+	sta $d020
+
+; make sure vicii color memory bank 1 is mapped in
+	lda $01
+	ora #$03
+	sta $01
+
+; fill vicii color memory with '5'
+	lda #$05
+	jsr fillit
+
+; make sure vicii color memory bank 0 is mapped in
+	lda $01
+	and #$fc
+	sta $01
+
+; fill vicii color memory with 'a'
+	lda #$0a
+	jsr fillit
+
+; set $00 and $01 in preparation for switching to c64 mode, and make sure vicii color memory bank 0 is the current one
+	lda #$36
+	sta $01
+	lda #$2f
+	sta $00
+
+; jump to $2000
+	jmp $2000
+
+
+; fill in vicii color memory with a value
+fillit:
+	ldx #$00
+fill_loop:
+	sta $d800,x
+	sta $d900,x
+	sta $da00,x
+	sta $db00,x
+	inx
+	bne fill_loop
+	rts
+
+*=$2000
+ 
+!binary "c64modez80-04.bin"
---
+++ c128/c64modemmu/c64modez80-04.c
@@ -0,0 +1,107 @@
+#include <stdlib.h>
+
+void main()
+{
+
+#asm
+	/* set the border color to indicate we made it into z80 mode */
+	ld a,1
+	ld bc,0xd020
+	out (c),a
+
+	/* check if we can access the z80 bios */
+	ld bc,0x048d
+	ld a,(bc)
+	cp 0x43
+	jr nz,noz80bios
+
+	inc bc
+	ld a,(bc)
+	cp 0x50
+	jr nz,noz80bios
+
+	inc bc
+	ld a,(bc)
+	cp 0x4d
+	jr z,z80bios_found
+
+noz80bios:
+	jr nz,noz80bios
+
+z80bios_found:
+	/* set the border color to indicate we found the z80 bios in c128 mode */
+	ld a,3
+	ld bc,0xd020
+	out (c),a
+
+     /* get the mmu version register in z80 reg D */
+	ld bc,0xd50b
+	in d,(c)
+
+	/* switch into c64 mode */
+	ld a,0xf6
+	ld bc,0xd505
+	out (c),a
+
+	/* get the mmu version register in z80 reg A */
+	ld bc,0xd50b
+	in a,(c)
+	cp a,d
+	jr nz,in_c64mode
+
+noc64mode:
+	jr z,noc64mode
+
+in_c64mode:
+
+	/* check what we find in the vicii color memory at $d800-$dbff */
+	ld d,0
+	ld e,0
+	ld bc,0xd800
+
+	in a,(c)
+	and 0xf
+	ld d,a
+	inc bc
+
+check_d800_loop:
+	in a,(c)
+	and 0xf
+	cp d
+	jr nz,wtf
+	inc bc
+	ld a,c
+	cp 0
+	jr nz,check_d800_loop
+	ld a,b
+	cp 0xdc
+	jr nz,check_d800_loop
+
+got_d800:
+	ld a,d
+	cp 5
+	jr z,got_5
+	cp 0xa
+	jr z,got_a
+wtf:
+	ld a,4
+	jr set_border
+
+got_5:
+	ld a,7
+	jr set_border
+
+got_a:
+	ld a,6
+
+set_border:
+	ld bc,0xd020
+	out (c),a
+
+	ld a,1
+	cp 1
+justloop:
+	jr z,justloop
+
+#endasm
+}
