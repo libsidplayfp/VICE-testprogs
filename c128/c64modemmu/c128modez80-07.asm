21b6a89a6f43775cda1da9e52b85dd258bc0274e
Marco van den Heuvel <blackystardust@users.sourceforge.net>
2023-02-27 02:31:29 +0000

    Added a c128 mode test to see if the z80 vicii color memory memory mapping is still accessable when /usr/bin/bash000-fff is shared memory.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/testprogs@43324 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ c128/c64modemmu/c128modez80-07.asm
@@ -0,0 +1,66 @@
+; This is a c128 mode test to see if the z80 vicii color memory memory mapping is still accessable when $0000-$1fff is shared memory.
+;
+; test to be confirmed on real hardware
+;
+; colors:
+;   black  = was not able to switch on the z80
+;   white  = got z80 switched on, but no z80 bios present
+;   cyan   = z80 on, z80 bios present in c128 mode, $1000-$13ff memory mapping present in shared memory
+;   violet = z80 on, z80 bios present in c128 mode, $1000-$13ff memory mapping NOT present in shared memory
+;   blue   = z80 on, z80 bios present in c128 mode, but something very wrong
+;
+; Test made by Marco van den Heuvel
+
+
+start=$1c40
+
+basicHeader=1 
+
+!ifdef basicHeader {
+; 10 SYS7181
+*=$1c01
+	!byte  $0c,$08,$0a,$00,$9e,$37,$31,$38,$31,$00,$00,$00
+*=$1c0d 
+	jmp start
+}
+*=start
+; clear the screen
+	lda #$93
+	jsr $ffd2
+
+	sei
+
+; $0000-$1fff shared memory
+	lda #$06
+	sta $d506
+
+; bank in bank 0 and bank in I/O
+	lda #$00
+	sta $ff00
+
+; make sure bank 1 vicii color memory is mapped in
+	lda $01
+	ora #$03
+	sta $01
+
+; fill vicii color memory with data
+	ldx #$00
+fill_loop:
+	txa
+	sta $d800,x
+	sta $d900,x
+	sta $da00,x
+	sta $db00,x
+	inx
+	bne fill_loop
+
+; change the border color to black
+	lda #$00
+	sta $d020
+
+; jump to $2000
+	jmp $2000
+
+*=$2000
+ 
+!binary "c128modez80-07.bin"
