43b8cf2534ff50084e5e65e1659bc8f411836d58
Marco van den Heuvel <blackystardust@users.sourceforge.net>
2023-03-02 03:25:06 +0000

    Added a c128 mode test to see if in z80 mode the mmu register 0 is readable through in/out d500 and memory access ff00.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/testprogs@43368 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ c128/c64modemmu/c128modez80-10.asm
@@ -0,0 +1,49 @@
+; This is a c128 mode test to see if in z80 mode the mmu register 0 is readable through in/out $d500 and memory access $ff00.
+;
+; test to be confirmed on real hardware
+;
+; colors:
+;   black  = was not able to switch on the z80
+;   white  = got z80 switched on, but no z80 bios present
+;   cyan   = z80 on, z80 bios present in c128 mode, mmu register 0 readable through in/out $d500 ONLY
+;   violet = z80 on, z80 bios present in c128 mode, mmu register 0 readable through in/out $d500 AND memory access $ff00
+;
+; Test made by Marco van den Heuvel
+
+
+start=$1c40
+
+basicHeader=1 
+
+!ifdef basicHeader {
+; 10 SYS7181
+*=$1c01
+	!byte  $0c,$08,$0a,$00,$9e,$37,$31,$38,$31,$00,$00,$00
+*=$1c0d 
+	jmp start
+}
+*=start
+; clear the screen
+	lda #$93
+	jsr $ffd2
+
+	sei
+
+; no shared memory
+	lda #$00
+	sta $d506
+
+; bank in bank 0 and bank in I/O
+	lda #$3e
+	sta $ff00
+
+; change the border color to black
+	lda #$00
+	sta $d020
+
+; jump to $2000
+	jmp $2000
+
+*=$2000
+ 
+!binary "c128modez80-10.bin"
