cd7cb2f1bc6d325a5adff0b07d7a29092917b2f8
Marco van den Heuvel <blackystardust@users.sourceforge.net>
2023-03-03 07:06:48 +0000

    Added a c128 mode test to see if in z80 mode the mmu PCRA, PCRB, PCRC and PCRD are accessable through memory access to ff01-ff04.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/testprogs@43381 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ c128/c64modemmu/c128modez80-12.asm
@@ -0,0 +1,50 @@
+; This is a c128 mode test to see if in z80 mode the mmu PCRA, PCRB, PCRC and PCRD are accessable through memory access to $ff01-$ff04.
+;
+; test to be confirmed on real hardware
+;
+; colors:
+;   black  = was not able to switch on the z80
+;   white  = got z80 switched on, but no z80 bios present
+;   cyan   = z80 on, z80 bios present in c128 mode, NONE of the $ff01-$ff04 mmu registers work
+;   violet = z80 on, z80 bios present in c128 mode, SOME of the $ff01-$ff04 mmu registers work
+;   blue   = z80 on, z80 bios present in c128 mode, ALL of the $ff01-$ff04 mmu registers work
+;
+; Test made by Marco van den Heuvel
+
+
+start=$1c40
+
+basicHeader=1 
+
+!ifdef basicHeader {
+; 10 SYS7181
+*=$1c01
+	!byte  $0c,$08,$0a,$00,$9e,$37,$31,$38,$31,$00,$00,$00
+*=$1c0d 
+	jmp start
+}
+*=start
+; clear the screen
+	lda #$93
+	jsr $ffd2
+
+	sei
+
+; no shared memory
+	lda #$00
+	sta $d506
+
+; bank in bank 0 and bank in I/O
+	lda #$3e
+	sta $ff00
+
+; change the border color to black
+	lda #$00
+	sta $d020
+
+; jump to $2000
+	jmp $2000
+
+*=$2000
+ 
+!binary "c128modez80-12.bin"
---
+++ c128/c64modemmu/c128modez80-12.c
@@ -0,0 +1,137 @@
+#include <stdlib.h>
+
+void main()
+{
+
+#asm
+	/* set the border color to indicate we made it into z80 mode */
+	ld a,1
+	ld bc,0xd020
+	out (c),a
+
+	/* check if we can access the z80 bios */
+	ld bc,0x048d
+	ld a,(bc)
+	cp 0x43
+	jr nz,noz80bios
+
+	inc bc
+	ld a,(bc)
+	cp 0x50
+	jr nz,noz80bios
+
+	inc bc
+	ld a,(bc)
+	cp 0x4d
+	jr z,z80bios_found
+
+noz80bios:
+	jr nz,noz80bios
+
+z80bios_found:
+	/* set the border color to indicate we found the z80 bios in c128 mode */
+	ld a,3
+	ld bc,0xd020
+	out (c),a
+
+/* prepare $d501-$d504 mmu registers */
+	ld bc,0xd501
+	ld a,0x3c
+	out (c),a
+	inc bc
+	ld a,0x38
+	out (c),a
+	inc bc
+	ld a,0x34
+	out (c),a
+	inc bc
+	ld a,0x30
+	out (c),a
+
+/* set $d500 to #$3e */
+	ld bc,0xd500
+	ld a,0x3e
+	out (c),a
+
+/* zero out the 'working' 'flag' (register d) */
+	ld d,0
+
+/* trigger PCRA at $ff01 */
+check_ff01:
+	ld bc,0xff01
+	ld a,0xff
+	ld (bc),a
+
+/* check if $d500 is now #$3c */
+	ld bc,0xd500
+	in a,(c)
+	cp 0x3c
+	jr nz,check_ff02
+	inc d
+
+/* trigger PCRB at $ff02 */
+check_ff02:
+	ld bc,0xff02
+	ld a,0xff
+	ld (bc),a
+
+/* check if $d500 is now #$38 */
+	ld bc,0xd500
+	in a,(c)
+	cp 0x38
+	jr nz,check_ff03
+	inc d
+
+/* trigger PCRC at $ff03 */
+check_ff03:
+	ld bc,0xff03
+	ld a,0xff
+	ld (bc),a
+
+/* check if $d500 is now #$34 */
+	ld bc,0xd500
+	in a,(c)
+	cp 0x34
+	jr nz,check_ff04
+	inc d
+
+/* trigger PCRC at $ff04 */
+check_ff04:
+	ld bc,0xff04
+	ld a,0xff
+	ld (bc),a
+
+/* check if $d500 is now #$30 */
+	ld bc,0xd500
+	in a,(c)
+	cp 0x30
+	jr nz,result_time
+	inc d
+
+result_time:
+	ld a,d
+	cp 0
+	jr z,none
+	cp 4
+	jr z,all
+
+some:
+	ld a,4
+	jr set_border
+
+none:
+	ld a,3
+	jr set_border
+
+all:
+	ld a,6
+
+set_border:
+	ld bc,0xd020
+	out (c),a
+
+justloop:
+	jr justloop
+
+#endasm
+}
