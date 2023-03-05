7542d5bb50e992fb1132322d28c5df9068ce503c
Marco van den Heuvel <blackystardust@users.sourceforge.net>
2023-03-05 05:01:11 +0000

    Added a c128 mode z80 test to see what happens if we set c000-ffff as shared memory and we access that memory with bank 1 banked in with in/out, do we get the ram from bank 0 or bank 1.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/testprogs@43411 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ c128/c64modemmu/c128modez80-13.asm
@@ -0,0 +1,71 @@
+; This is a c128 mode test to see what happens if we set $c000-$ffff as shared memory and we access that memory with bank 1 banked in with in/out,
+; do we get the ram from bank 0 or bank 1.
+;
+; test to be confirmed on real hardware
+;
+; colors:
+;   black  = was not able to switch on the z80
+;   white  = got z80 switched on, but no z80 bios present
+;   cyan   = z80 on, z80 bios present in c128 mode, we get RAM from bank 1
+;   violet = z80 on, z80 bios present in c128 mode, we get RAM from bank 0
+;   red    = something went very wrong during the test
+;
+; Test made by Marco van den Heuvel
+
+
+start=$1c40
+
+basicHeader=1 
+
+!ifdef basicHeader {
+; 10 SYS7181
+*=$1c01
+	!byte  $0c,$08,$0a,$00,$9e,$37,$31,$38,$31,$00,$00,$00
+*=$1c0d 
+	jmp start
+}
+*=start
+; clear the screen
+	lda #$93
+	jsr $ffd2
+
+	sei
+
+; set $0000-$3fff as shared memory
+	lda #$07
+	sta $d506
+
+; bank in bank 1 and bank in I/O
+	lda #$7e
+	sta $ff00
+
+; put #$55 in $c000 in bank 1
+	lda #$55
+	sta $c000
+
+; bank in bank 0 and bank in I/O
+	lda #$3e
+	sta $ff00
+
+; put #$aa in $c000 in bank 0
+	lda #$aa
+	sta $c000
+
+; set $0000-$3fff and $c000-$ffff as shared memory
+	lda #$0f
+	sta $d506
+
+; bank in bank 0 and bank in I/O
+	lda #$7e
+	sta $ff00
+
+; change the border color to black
+	lda #$00
+	sta $d020
+
+; jump to $2000
+	jmp $2000
+
+*=$2000
+ 
+!binary "c128modez80-13.bin"
---
+++ c128/c64modemmu/c128modez80-13.c
@@ -0,0 +1,81 @@
+#include <stdlib.h>
+
+void main()
+{
+
+#asm
+	/* set the border color to indicate we made it into z80 mode */
+	ld a,1
+	ld bc,0xd020
+	out (c),a
+
+	/* check if we can access the z80 bios */
+	ld bc,0x048d
+	ld a,(bc)
+	cp 0x43
+	jr nz,noz80bios
+
+	inc bc
+	ld a,(bc)
+	cp 0x50
+	jr nz,noz80bios
+
+	inc bc
+	ld a,(bc)
+	cp 0x4d
+	jr z,z80bios_found
+
+noz80bios:
+	jr nz,noz80bios
+
+z80bios_found:
+	/* set the border color to indicate we found the z80 bios in c128 mode */
+	ld a,3
+	ld bc,0xd020
+	out (c),a
+
+/* bank in bank 0 and I/O, the rest RAM */
+	ld a,0x3e
+	ld bc,0xff00
+	ld (bc),a
+
+/* set $0000-$3fff and $c000-$ffff as shared memory */
+	ld a,0xf
+	ld bc,0xd506
+	out (c),a
+
+/* bank in bank 1 and I/O, the rest RAM */
+	ld a,0x7e
+	ld bc,0xff00
+	ld (bc),a
+
+/* read from $c000 using in/out */
+	ld bc,0xc000
+	in a,(c)
+
+	cp 0x55
+	jr z,bank1
+
+	cp 0xaa
+	jr z,bank0
+
+wtf:
+	ld a,2
+	jr set_border
+
+bank1:
+	ld a,3
+	jr set_border
+
+bank0:
+	ld a,4
+
+set_border:
+	ld bc,0xd020
+	out (c),a
+
+justloop:
+	jr justloop
+
+#endasm
+}
