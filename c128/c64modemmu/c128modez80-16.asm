99581ce54839dfcbd43f6c6cd69ecaeabd428e65
Marco van den Heuvel <blackystardust@users.sourceforge.net>
2023-03-06 14:57:42 +0000

    Added a c128 mode z80 test to see what we get if we access 04xx through i/o using in/out when the mmu i/o bit is on, do we get 04xx bios, do we get 04xx ram or do we get d4xx ram.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/testprogs@43427 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ c128/c64modemmu/c128modez80-16.asm
@@ -0,0 +1,80 @@
+; This is a c128 mode z80 test to see what we get if we access $04xx through i/o using in/out when the mmu i/o bit is on,
+; do we get $04xx bios, do we get $04xx ram or do we get $d4xx ram.
+;
+; test to be confirmed on real hardware
+;
+; colors:
+;   black  = was not able to switch on the z80
+;   white  = got z80 switched on, but no z80 bios present
+;   cyan   = z80 on, z80 bios present in c128 mode, we got $04xx BIOS
+;   violet = z80 on, z80 bios present in c128 mode, we got $04xx RAM
+;   blue   = z80 on, z80 bios present in c128 mode, we got $d4xx RAM
+;   red    = something went very wrong during the test
+;
+; Test made by Marco van den Heuvel
+
+
+start=$1c40
+
+basicHeader=1 
+
+!ifdef basicHeader {
+; 10 SYS7181
+*=$1c01
+	!byte  $0c,$08,$0a,$00,$9e,$37,$31,$38,$31,$00,$00,$00
+*=$1c0d 
+	jmp start
+}
+*=start
+; clear the screen
+	lda #$93
+	jsr $ffd2
+
+	sei
+
+; no shared memory
+	lda #$00
+	sta $d506
+
+; bank in bank 0 and bank out I/O
+	lda #$3f
+	sta $ff00
+
+; put #$55 in $048d
+	lda #$55
+	sta $048d
+
+; put #$aa in $048e
+	lda #$aa
+	sta $048e
+
+; put #$33 in $048f
+	lda #$33
+	sta $048f
+
+; put #$66 in $d48d ram
+	lda #$66
+	sta $d48d
+
+; put #$99 in $d48e ram
+	lda #$99
+	sta $d48e
+
+; put #$cc in $d48f ram
+	lda #$cc
+	sta $d48f
+
+; bank in bank 0 and bank in I/O
+	lda #$3e
+	sta $ff00
+
+; change the border color to black
+	lda #$00
+	sta $d020
+
+; jump to $2000
+	jmp $2000
+
+*=$2000
+ 
+!binary "c128modez80-16.bin"
---
+++ c128/c64modemmu/c128modez80-16.c
@@ -0,0 +1,142 @@
+#include <stdlib.h>
+
+void main()
+{
+
+#asm
+	/* set the border color to indicate we made it into z80 mode */
+	ld a,1
+	ld bc,0xd020
+	out (c),a
+
+	/* check if we can access the z80 bios */
+	ld bc,0x048d
+	ld a,(bc)
+	cp 0x43
+	jr nz,noz80bios
+
+	inc bc
+	ld a,(bc)
+	cp 0x50
+	jr nz,noz80bios
+
+	inc bc
+	ld a,(bc)
+	cp 0x4d
+	jr z,z80bios_found
+
+noz80bios:
+	jr nz,noz80bios
+
+z80bios_found:
+	/* set the border color to indicate we found the z80 bios in c128 mode */
+	ld a,3
+	ld bc,0xd020
+	out (c),a
+
+/* reset the z80bios (d) and $04xx ram (e) and $d4xx ram (h) 'flags' */
+	ld d,0
+	ld e,0
+	ld h,0
+
+/* read from $048d memory */
+	ld bc,0x048d
+	in a,(c)
+	cp 0x43
+	jr z,got_048d_bios
+	cp 0x55
+	jr z,got_048d_ram
+	cp 0x66
+	jr z,got_d48d_ram
+	jr wtf
+
+got_048d_bios:
+	inc d
+	jr check_048e
+
+got_048d_ram:
+	inc e
+	jr check_048e
+
+got_d48d_ram:
+	inc h
+
+check_048e:
+	inc bc
+	in a,(c)
+	cp 0x50
+	jr z,got_048e_bios
+	cp 0xaa
+	jr z,got_048e_ram
+	cp 0x99
+	jr z,got_d48e_ram
+	jr wtf
+
+got_048e_bios:
+	inc d
+	jr check_048f
+
+got_048e_ram:
+	inc e
+	jr check_048f
+
+got_d48e_ram:
+	inc h
+
+check_048f:
+	inc bc
+	in a,(c)
+	cp 0x4d
+	jr z,got_048f_bios
+	cp 0x33
+	jr z,got_048f_ram
+	cp 0xcc
+	jr z,got_d48f_ram
+	jr wtf
+
+got_048f_bios:
+	inc d
+	jr final_results
+
+got_048f_ram:
+	inc e
+	jr final_results
+
+got_d48f_ram:
+	inc h
+
+final_results:
+	ld a,d
+	cp 3
+	jr z,got_04xx_bios
+	ld a,e
+	cp 3
+	jr z,got_04xx_ram
+	ld a,h
+	cp 3
+	jr z,got_d4xx_ram
+
+wtf:
+	ld a,2
+	jr set_border
+
+got_04xx_bios:
+	ld a,3
+	jr set_border
+
+got_04xx_ram:
+	ld a,4
+	jr set_border
+
+got_d4xx_ram:
+	ld a,6
+
+set_border:
+	ld bc,0xd020
+	out (c),a
+
+justloop:
+	jr justloop
+
+#endasm
+}
