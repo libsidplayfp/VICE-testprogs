119b8ca6cc0fc92b40e6e2af34d00386826dcc6a
Marco van den Heuvel <blackystardust@users.sourceforge.net>
2023-03-11 21:41:36 +0000

    Changed border color for tests to green according to the results on actual hw, and added vice debug cart values.
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/testprogs@43498 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- c128/c64modemmu/c128modez80-25.asm
+++ c128/c64modemmu/c128modez80-25.asm
@@ -1,13 +1,13 @@
 ; This is a c128 mode z80 test to see what happens when we use the z80 memory access to change the ram in $0100 when z80 bios is mapped in.
 ;
-; test to be confirmed on real hardware
+; test confirmed on real hardware
 ;
 ; colors:
 ;   black  = was not able to switch on the z80
 ;   white  = got z80 switched on, but no z80 bios present
 ;   cyan   = z80 on, z80 bios present in c128 mode, when using z80 it changed NOTHING
 ;   violet = z80 on, z80 bios present in c128 mode, when using z80 it changed RAM at $0100 ONLY
-;   blue   = z80 on, z80 bios present in c128 mode, when using z80 it changed RAM at $d100 ONLY
+;   green  = z80 on, z80 bios present in c128 mode, when using z80 it changed RAM at $d100 ONLY
 ;   yellow = z80 on, z80 bios present in c128 mode, when using z80 it changed RAM at $0100 AND $d100
 ;
 ; Test made by Marco van den Heuvel
@@ -100,23 +100,28 @@ final_results:
 
 ; no changes at all
 	ldx #$03
+	ldy #$ff
 	bne set_border
 
 changed_0100:
 	ldx #$04
+	ldy #$ff
 	bne set_border
 
 changed_d100:
-	ldx #$06
-	bne set_border
+	ldx #$05
+	ldy #$00
+	beq set_border
 
 changed_0100_and_d100:
 	ldx #$07
+	ldy #$ff
 
 set_border:
 	lda #$3e
 	sta $ff00
 	stx $d020
+	sty $d7ff
 	clc
 l0:
 	bcc l0
