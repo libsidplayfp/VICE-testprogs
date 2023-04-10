f041e334c2e0eab6a514641f88aa91703c9c1a63
Ingo Korb <ikorb@users.sourceforge.net>
2023-04-10 11:01:46 +0000

    test more open bus space in drive/openbus
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/testprogs@43657 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- drive/openbus/main.asm
+++ drive/openbus/main.asm
@@ -32,7 +32,7 @@ start:
 
         jsr rcv_wait
 
-        ; recieve the result data
+        ; recieve the indirect RAM read result data
         ldx #$01
 -
         jsr rcv_1byte
@@ -40,23 +40,90 @@ start:
         inx
         bne -
 
-        ; check data
+        ; receive the high-byte read result data
+        ldx #$08
+-
+        jsr rcv_1byte
+        sta $0600,x
+        inx
+        cpx #$78
+        bne -
+
+        ; blank out ignored data
+        lda #$20
+        ldx #$0f
+-
+        sta $0600 + $18,x
+        sta $0600 + $38,x
+        sta $0600 + $58,x
+        dex
+        bpl -
+
+        ; check indirect RAM read data
         ldx #$00
 -
         txa
         cmp $0401,x
-        bne chkerr
+        bne chkerr1
         inx
         cpx #$ff
         bne -
 
+        ; check high-byte read data 08xx-17xx
+        ldx #$08
+-
+        txa
+        cmp $0600,x
+        bne chkerr2
+        inx
+        cpx #$18
+        bne -
+
+        ; check high-byte read data 28xx-37xx
+        ldx #$28
+-
+        txa
+        cmp $0600,x
+        bne chkerr2
+        inx
+        cpx #$38
+        bne -
+
+        ; check high-byte read data 48xx-57xx
+        ldx #$48
+-
+        txa
+        cmp $0600,x
+        bne chkerr2
+        inx
+        cpx #$58
+        bne -
+
+        ; check high-byte read data 68xx-77xx
+        ldx #$68
+-
+        txa
+        cmp $0600,x
+        bne chkerr2
+        inx
+        cpx #$78
+        bne -
+
+        ; all ok
         lda #5
         sta $d020
         jmp waitkey
-chkerr:
+
+chkerr1:
         lda #10
         sta $d020
         sta $d801,x             ; highlight mismatch
+        jmp waitkey
+
+chkerr2:
+        lda #10
+        sta $d020
+        sta $da00,x             ; highlight mismatch
         jmp waitkey
 
 waitkey:
@@ -112,15 +179,37 @@ drvstart
         sta .readdata,x
         inx
         bne -
+
+        ; some extra probes for high-byte-still-on-bus
+        ; RAM/VIA mirrors will be skipped during validation on C64 side
+        ldy #$23                ; arbitrary offset, must be less than $80
+        sty $14
+        ldy #$08
+-
+        sty $15
+        lda ($14),y
+        sta .wrdata,y
+        iny
+        cpy #$78
+        bne -
         
         jsr snd_start
 
-        ; send test data
+        ; send indirect read test data
         ldy #$01
 -
         lda .readdata,y
         jsr snd_1byte
         iny
+        bne -
+
+        ; send high-byte-still-on-bus test data
+        ldy #$08
+-
+        lda .wrdata,y
+        jsr snd_1byte
+        iny
+        cpy #$78
         bne -
 
         ;rts
--- drive/openbus/readme.txt
+++ drive/openbus/readme.txt
@@ -1,5 +1,7 @@
 Checks open bus values by forcing known data from ram with
-the dummy cycle on an indexing overflow (lda $07ff,x with x >=1).
+the dummy cycle on an indexing overflow (lda $07ff,x with x >=1)
+and reading the last instruction byte in open areas from 0800-7f00
+(one byte per page only).
 
 Returns green on my OC118, expected to work on real 1541s too,
-but not yet tested. Will fail on a 1581 due to larger RAM.
+but not yet tested. Will fail on a 1581.
