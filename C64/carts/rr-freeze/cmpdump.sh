03776377cab69333873c74664a4b26c9f46be98c
Daniel Kahlin <tlr@users.sourceforge.net>
2024-04-06 12:24:27 +0000

    allow selecting meld for diffs
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/testprogs@45090 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- C64/carts/rr-freeze/cmpdump.sh
+++ C64/carts/rr-freeze/cmpdump.sh
@@ -1,6 +1,22 @@
 #! /bin/bash
-./analyze $1 > /tmp/dump1.txt
-./analyze $2 > /tmp/dump2.txt
-diff -y /tmp/dump1.txt /tmp/dump2.txt
+
+# get arguments
+if [ "$1" = "-m" ]; then
+    meld=true
+    shift
+fi
+src1=$1
+src2=$2
+
+# analyze
+./analyze $src1 > /tmp/dump1.txt
+./analyze $src2 > /tmp/dump2.txt
+
+# present results
+if [ "$meld" = "true" ]; then
+    meld /tmp/dump1.txt /tmp/dump2.txt
+else
+    diff -y -W 160 /tmp/dump1.txt /tmp/dump2.txt
+fi
 
 # eof
