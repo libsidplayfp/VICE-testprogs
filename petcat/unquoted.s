f5fbb8f202ca0fc221160d587629c61a6c3fd9cb
groepaz <gpz@users.sourceforge.net>
2026-01-23 23:39:59 +0000

    add round trip checks for all codes, quoted and non quoted
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/testprogs@45961 379a1393-f5fb-40a0-bcee-ef074d9b53f7

---
+++ petcat/unquoted.s
@@ -0,0 +1,45 @@
+        fillvalue=$00
+
+        !initmem 	fillvalue
+        !cpu 6502
+        !to "unquoted.prg", cbm
+
+; all codes, outside quotes, one per line
+
+        *= $0801
+
+        !for i, 255 {
+            !if (i != $20) {
+            !word * + 6     ; ptr to next line
+            !word i         ; line nr
+            !byte i         ; value
+            !byte 0         ; end of line
+            }
+        }
+
+; all codes after DATA, outside quotes, one per line
+        !for i, 255 {
+            !if (i != $20) {
+            !word * + 8         ; ptr to next line
+            !word 1000 + i      ; line nr
+            !byte $83           ; DATA
+            !byte $20, i        ; space, value
+            !byte 0             ; end of line
+            }
+        }
+
+; all codes after REM, outside quotes, one per line
+        !for i, 255 {
+            !if (i != $20) {
+            !word * + 8         ; ptr to next line
+            !word 2000 + i      ; line nr
+            !byte $8f           ; REM
+            !byte $20, i        ; space, value
+            !byte 0             ; end of line
+            }
+        }
+
+        !word 0         ; basic end
+
+; FIXME: a line containing a single space is not handled
+; FIXME: space(s) at the end of a line are not handled
