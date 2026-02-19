8b32839f8bbffa9ffa3fabd2de4edaddf85c8f59
groepaz <gpz@users.sourceforge.net>
2026-02-19 21:53:01 +0000

    updated test(s), add extra test for multiple spaces at the start and end of a line
    git-svn-id: https://svn.code.sf.net/p/vice-emu/code/testprogs@45974 379a1393-f5fb-40a0-bcee-ef074d9b53f7

--- petcat/quoted.s
+++ petcat/quoted.s
@@ -9,37 +9,28 @@
         *= $0801
 
         !for i, 255 {
-            !if (i != $20) {
             !word * + 8     ; ptr to next line
             !word i         ; line nr
             !byte $22,i,$22 ; value
             !byte 0         ; end of line
-            }
         }
 
 ; all codes after DATA, outside quotes, one per line
         !for i, 255 {
-            !if (i != $20) {
             !word * + 9         ; ptr to next line
             !word 1000 + i      ; line nr
             !byte $83           ; DATA
             !byte $22, i, $22   ; space, value
             !byte 0             ; end of line
-            }
         }
 
 ; all codes after REM, outside quotes, one per line
         !for i, 255 {
-            !if (i != $20) {
             !word * + 9         ; ptr to next line
             !word 2000 + i      ; line nr
             !byte $8f           ; REM
             !byte $22, i, $22   ; space, value
             !byte 0             ; end of line
-            }
         }
 
         !word 0         ; basic end
-
-; FIXME: a line containing a single space is not handled
-; FIXME: space(s) at the end of a line are not handled
---
+++ petcat/spaces.s
@@ -0,0 +1,37 @@
+
+
+
+	fillvalue=$00
+
+	!initmem 	fillvalue
+	!cpu 6502
+	!to "spaces.prg", cbm
+
+        *= $0801
+line0:
+        !word line1 ; ptr to next line
+        !word 0     ; line nr
+
+        !byte $20, $20, $20
+        !pet "3 spaces at start of line"
+        !byte 0 ; end of line
+line1:
+        !word line2 ; ptr to next line
+        !word 1     ; line nr
+
+        !pet "3 spaces:"
+        !byte $20, $20, $20
+;        !pet "mid-line"
+        !pet ":here"
+        !byte 0 ; end of line
+line2:
+        !word line3 ; ptr to next line
+        !word 2     ; line nr
+
+        !pet "3 spaces at end of line"
+        !byte $20, $20, $20
+        !byte 0 ; end of line
+line3:
+
+
+    !word 0 ; basic end
